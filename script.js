43914e87837d0368935579f3e83d702feae0cb14
tabularization <127825421+tabularization@users.noreply.github.com>
2023-09-18 12:44:42 -0400

    Revamps

--- script.js
+++ script.js
@@ -22,8 +22,21 @@ document.addEventListener("DOMContentLoaded", function () {
         // Use the targetId to find the corresponding element
         const targetElement = document.querySelector(targetId);
         
-        targetElement.scrollIntoView({ behavior: "smooth" });
+        targetElement.scrollIntoView({ behavior: "smooth", block: "start", inline:"nearest"});
       });
     });
   });
   
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show');
+      }
+    });
+  });
+
+  const hiddenElements = document.querySelectorAll('.hidden');
+  hiddenElements.forEach((el) => observer.observe(el));
